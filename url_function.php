fb165977b9e483306af9ea137461c087af8cd741
Rising13 <ravenqz13@gmail.com>
2018-10-16 13:24:23 +0200

    Update url_function.php
    Поправил функцию request_url

--- url_function.php
+++ url_function.php
@@ -2,26 +2,22 @@
 /*Функция получения текущего url страницы*/
 function request_url()
 {
-  /*Значение по умолчанию*/
   $result = '';
   $default_port = 80; // Порт по-умолчанию
  
-  // Тип соединения?
+  // Тип соединения
   if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS']=='on')) {
-    // В защищенном! Добавим протокол...
     $result .= 'https://';
-    // Переназначим значение порта по-умолчанию
     $default_port = 443;
   } else {
-    // Обычное соединение, обычный протокол
     $result .= 'http://';
   }
+	
   // Имя сервера
   $result .= $_SERVER['SERVER_NAME'];
  
   // порт
   if ($_SERVER['SERVER_PORT'] != $default_port) {
-    // Если нет, то добавим порт в URL
     $result .= ':'.$_SERVER['SERVER_PORT'];
   }
   // путь и GET-параметры.
